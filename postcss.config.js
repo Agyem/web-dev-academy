4c134544e2ad92097a779870fb145093e79c91c2
nharna Agyeman <159562322+Agyem@users.noreply.github.com>
2025-12-19 00:08:21 +0000

    Add PostCSS configuration file

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'postcss-nested': {},
+    'autoprefixer': {},
+    'cssnano': {
+      preset: ['default', {
+        discardComments: {
+          removeAll: true,
+        },
+      }],
+    },
+  },
+};
